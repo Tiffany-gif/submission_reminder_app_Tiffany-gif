81e5e952abc82131dd25f0411d70e8bf4453c8f4
Your Name <you@example.com>
2025-02-14 07:11:36 -0800

    Submissions Reminder App setup

--- create_environment.sh
+++ create_environment.sh
@@ -28,8 +28,25 @@ echo "# Configuration File" > "$base_dir/config/config.env"
 echo "REMINDER_INTERVAL=24h" >> "$base_dir/config/config.env"
 
 # Populate reminder.sh script
-echo "#!/bin/bash" > "$base_dir/app/reminder.sh"
-echo "echo 'Sending reminders to students...'" >> "$base_dir/app/reminder.sh"
+cat <<EOL > "$base_dir/app/reminder.sh"
+#!/bin/bash
+
+
+#Source environment variables and helper functions
+source "\$(dirname "\$0")/../config/config.env"
+source "\$(dirname "\$0")/../modules/functions.sh"
+
+# Path to the submissions file
+submissions_file="$base_dir/assets/submissions.txt"
+
+# Print remaining time and run the reminder function
+echo "Assignment: \$ASSIGNMENT"
+echo "Days remaining to submit: \$DAYS_REMAINING days"
+echo "--------------------------------------------"
+
+check_submissions "\$submissions.file"
+EOL
+
 chmod +x "$base_dir/app/reminder.sh"
 
 # Populate functions.sh script
@@ -40,11 +57,12 @@ echo "}" >> "$base_dir/modules/functions.sh"
 chmod +x "$base_dir/modules/functions.sh"
 
 # Populate submissions.txt with sample data
-echo "John Doe, Submitted" > "$base_dir/assets/submissions.txt"
-echo "Jane Smith, Not Submitted" >> "$base_dir/assets/submissions.txt"
-echo "Alice Brown, Submitted" >> "$base_dir/assets/submissions.txt"
-echo "Bob White, Not Submitted" >> "$base_dir/assets/submissions.txt"
-echo "Charlie Green, Submitted" >> "$base_dir/assets/submissions.txt"
+echo "student, assignment, submission status" > "$base_dir/assets/submissions.txt"i
+echo "Ashelia, Shell Navigation, Submitted" >> "$base_dir/assets/submissions.txt"
+echo "Emma, Shell Navigation, Not Submitted" >> "$base_dir/assets/submissions.txt"
+echo "Richy, Shell Navigation, Submitted" >> "$base_dir/assets/submissions.txt"
+echo "Shema, Shell Navigation, Not Submitted" >> "$base_dir/assets/submissions.txt"
+echo "Shaun, Shell Navigation, Submitted" >> "$base_dir/assets/submissions.txt"
 
 # Populate startup.sh script
 echo "#!/bin/bash" > "$base_dir/startup.sh"
--- submission_reminder_Tiffany/app/reminder.sh
+++ submission_reminder_Tiffany/app/reminder.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo 'Sending reminders to students...'
+
+
+#Source environment variables and helper functions
+source "$(dirname "$0")/../config/config.env"
+source "$(dirname "$0")/../modules/functions.sh"
+
+# Path to the submissions file
+submissions_file="submission_reminder_Tiffany/assets/submissions.txt"
+
+# Print remaining time and run the reminder function
+echo "Assignment: $ASSIGNMENT"
+echo "Days remaining to submit: $DAYS_REMAINING days"
+echo "--------------------------------------------"
+
+check_submissions "$submissions.file"
